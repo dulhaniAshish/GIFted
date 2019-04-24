1782e31f8b2d27aa378669bad64c0a5742bf1ffb
ASHISH DULHANI <ashishdulhani@novopay.in>
2019-04-24 13:24:08 +0530

    show shimmer effect

--- sw.js
+++ sw.js
@@ -1,9 +1,21 @@
-self.addEventListener('activate', event => {
-    event.waitUntil(clients.claim());
+self.addEventListener('activate', (event) => {
+    console.info('Event: Activate');
+    event.waitUntil(
+        self.clients.claim(),
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== 'v1') {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
 });
 
 self.addEventListener('fetch', event => {
-        // console.log(event);
+        if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
         caches.match(event.request).then(function (resp) {
             return resp || fetch(event.request).then(function (response) {
                 return caches.open('v1').then(function (cache) {
